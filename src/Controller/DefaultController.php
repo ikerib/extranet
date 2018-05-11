0af9d459ceedc83640bccdcc340e6e8fcb87b992
Iker Ibarguren Berasaluze <ikerib@gmail.com>
2018-05-11 14:33:02 +0200

    Homepage-rekin lanean

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,7 +3,11 @@
 namespace App\Controller;
 
 
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\File\Exception\FileNotFoundException;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Finder\Finder;
@@ -51,14 +55,14 @@ class DefaultController extends Controller
         $folders = $this->get( 'App\Controller\DefaultController' )->getSidebarFolders();
 
         return $this->render( 'default/index.html.twig', [
-            'breadcrumbs'=>[],
-            'currentDir'=>[],
-            'folders' => $folders,
-            'files'   => [],
-            'dirs'    => [],
+            'breadcrumbs' => [],
+            'currentDir'  => "/",
+            'folders'     => $folders,
+            'homeFolders' => $folders,
+            'files'       => [],
+            'dirs'        => null,
         ] );
     }
-
 
     /**
      * @Route("/finder/", name="dirpath")
@@ -71,6 +75,7 @@ class DefaultController extends Controller
     public function dirpath( Request $request )
     {
         $dirpath = $request->get( 'dirpath' );
+        $orden   = $request->get( 'orden' );
         $folders = $this->get( 'App\Controller\DefaultController' )->getSidebarFolders();
 
         $basedir = rtrim( getenv( 'APP_FOLDER_PATH' ), '/' );
@@ -78,19 +83,29 @@ class DefaultController extends Controller
 
 
         $folderFinder = new Finder();
-        $dirs         = $folderFinder->directories()->in( $myPath )->depth( '<1' )->sortByName();
+        if ( isset( $orden ) ) {
+            $dirs = $folderFinder->directories()->in( $myPath )->depth( '<1' )->sortByName();
+        } else {
+            if ( $orden == "name" ) {
+                $dirs = $folderFinder->directories()->in( $myPath )->depth( '<1' )->sortByName();
+            } elseif ( $orden == "created" ) {
+                $dirs = $folderFinder->directories()->in( $myPath )->depth( '<1' )->sortByChangedTime();
+            } elseif ( $orden == "updated" ) {
+                $dirs = $folderFinder->directories()->in( $myPath )->depth( '<1' )->sortByModifiedTime();
+            }
+        }
 
         $filesFinder = new Finder();
         $files       = $filesFinder->files()->in( $myPath );
 
-        $breadcrumbs = explode( '/', ltrim($dirpath, '/') );
-        $ogiazalak = [];
+        $breadcrumbs = explode( '/', ltrim( $dirpath, '/' ) );
+        $ogiazalak   = [];
 
-        foreach ($breadcrumbs as $key=>$value) {
-            if ($key == 0) {
+        foreach ( $breadcrumbs as $key => $value ) {
+            if ( $key == 0 ) {
                 $ogiazalak[ $value ] = $value;
             } else {
-                $ogiazalak[ $value ] = $breadcrumbs[ $key-1 ] . "/" . $value;
+                $ogiazalak[ $value ] = $breadcrumbs[ $key - 1 ] . "/" . $value;
             }
 
         }
@@ -102,6 +117,33 @@ class DefaultController extends Controller
             'dirs'        => $dirs,
             'files'       => $files,
         ] );
+
+    }
+
+    /**
+     * @Route("/finder/file/download", name="file_download")
+     *
+     *
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function fileDownload( Request $request )
+    {
+        $filePath = $request->get( 'filePath' );
+
+        /* Check if file exists */
+        /** @var Filesystem $fs */
+        $fs = new Filesystem();
+        if ( $fs->exists( $filePath ) ) {
+            $response = new BinaryFileResponse( $filePath );
+            $response->setContentDisposition( ResponseHeaderBag::DISPOSITION_ATTACHMENT );
+
+            return $response;
+        } else {
+            throw new FileNotFoundException();
+        }
+
 
     }
 }
