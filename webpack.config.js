56297ce264539616699c6c0532dc715b3c263e87
ikerib <ikerib@gmail.com>
2022-11-04 08:51:50 +0100

    Fix max upload size

--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,8 @@ Encore
     // .enableVersioning(Encore.isProduction())
 
     // uncomment to define the assets of the project
-    .addEntry('js/app', ['./assets/js/fontawesome-5.0.13.all.js','./assets/js/app.js'])
     .addEntry('js/dropzone', ['./assets/js/dropzone.js'])
+    .addEntry('js/app', ['./assets/js/fontawesome-5.0.13.all.js','./assets/js/app.js'])
 
 
     .addStyleEntry('css/app', ['./assets/css/app.scss', './assets/css/dashboard.scss'])
